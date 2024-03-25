3ad6aef88dc6271d4b3dd97fa1b8e721f2da0f04
jayroraldo <arjayroraldo@gmail.com>
2024-03-25 23:14:50 +0800

    add guard to prevent redirection without authorization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { WeatherComponent } from './components/weather/weather.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'weather', component: WeatherComponent },
+  { path: 'weather', component: WeatherComponent, canActivate: [AuthGuard] },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
