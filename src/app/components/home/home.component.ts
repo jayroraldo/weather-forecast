d552a52ef9496af444cc8fefc950ac7eca188e6e
jayroraldo <arjayroraldo@gmail.com>
2024-03-25 18:42:32 +0800

    move components to components folder

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../components/header/header.component';
-import { LoginComponent } from '../components/login/login.component';
+import { HeaderComponent } from '../header/header.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
